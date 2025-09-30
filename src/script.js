21ea1bad8c9f2e744f07a15db7a37869db91a23d
Aereen <eirene.armilla@gmail.com>
2025-10-01 00:21:08 +0800

    updated 10/01

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 //Text Typing Effect
-const phrases = ["Web Development", "Web Design", "Project Management"];
+const phrases = ["Web Development", "Web Design", "Project Management", "UI/UX Design"];
     const typingElement = document.getElementById("typing-text");
 
         let phraseIndex = 0;
@@ -33,3 +33,23 @@ const phrases = ["Web Development", "Web Design", "Project Management"];
             }
 
 window.addEventListener("DOMContentLoaded", type);
+
+// Modal Functionality
+    const openModal = document.getElementById("openModal");
+    const closeModal = document.getElementById("closeModal");
+    const modal = document.getElementById("capabilitiesModal");
+
+    openModal.addEventListener("click", () => {
+        modal.classList.remove("hidden");
+    });
+
+    closeModal.addEventListener("click", () => {
+        modal.classList.add("hidden");
+    });
+
+    // Close when clicking outside modal content
+    window.addEventListener("click", (e) => {
+        if (e.target === modal) {
+        modal.classList.add("hidden");
+        }
+    });
